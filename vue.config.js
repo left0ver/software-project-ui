7eeb965f9481263d7ee164a302a4ec9c57ef29a4
“left0ver” <1614674987@qq.com>
2022-03-16 17:45:59 +0800

    perf: make some library load by using CDN

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,22 @@ module.exports = {
 	devServer: {
 		disableHostCheck: true,
 	},
+
+	// 发布模式--发布阶段的打包入口
+	chainWebpack: config => {
+		config.when(process.env.NODE_ENV === "production", config => {
+			config.entry("app").clear().add("./src/main.js");
+			config.set("externals", {
+				vue: "Vue",
+				"vue-router": "VueRouter",
+				axios: "axios",
+				vuex: "Vuex",
+			});
+		});
+	},
+
+	configureWebpack: {
+		devtool: false,
+		mode: "production",
+	},
 };
